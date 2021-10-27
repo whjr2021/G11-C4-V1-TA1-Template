5ba987d711a191bb8705748ede4fe4a1034e72b8
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-27 13:13:29 +0530

    Update C4_TA1_Template.py

--- C4_TA1_Template.py
+++ C4_TA1_Template.py
@@ -1,7 +1,7 @@
-# TA 1a: Code to print the numbers 0 to 4 using a for loop
+# TA 1a: Code to print the numbers 0 to 4 using a for loop with range() function in the format range(start, stop)
 
 
 
 
-# TA 1b: Code to print x-coordinates of the bricks using a for loop
+# TA 1b: Code to print x-coordinates of the bricks using a for loop with range() function in the format range(stop)
 
